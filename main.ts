92d5a258cfa19edf9e7f8b7f4e27c6dcf47ea19e
subhashnaidu45 <117711902+subhashnaidu45@users.noreply.github.com>
2022-12-23 17:36:06 +0530

    1 file modified

--- main.ts
+++ main.ts
@@ -45,7 +45,7 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app-root';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   exports: [
